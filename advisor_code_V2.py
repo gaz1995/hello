b789f876060649a8e56ba83e95f62992d929af7f
gaz1995 <84091021+gaz1995@users.noreply.github.com>
2021-05-15 16:17:47 +0100

    Update advisor_code_V2.py

--- advisor_code_V2.py
+++ advisor_code_V2.py
@@ -12,7 +12,7 @@ from matplotlib.patches import Rectangle
 #Dashboard title
 st.title('Financial Planning Dashboard')
 
-st.markdown("The dashboard consists of two parts: \n1.Visulization \n2.Strategy Fundamentals")
+st.text("The dashboard consists of two parts: \n1.Visulization \n2.Strategy Fundamentals")
 st.markdown("1. Visulization")
 st.markdown("2. Strategy Fundamentals")
 
